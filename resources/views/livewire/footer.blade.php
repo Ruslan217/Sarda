104c27583eb4a2869a487225cfba897edce2abcc
Ruslan.Shamsudinov <ruslan.shamsudinov.217@gmail.com>
2024-08-01 20:15:47 +0400

    Commit Before DB Migration

---
+++ resources/views/livewire/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container">
+        <p class="copyright__text text-white fm-bold text-center fs-13 mb-0">
+            &copy; 2024 SARDA GROUP. ALL RIGHTS RESERVED.
+        </p>
+    </div>
+</footer>
---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,39 @@
+<div>
+    <livewire:header />
+
+    <main class="main bg-dark">
+        <div class="container">
+            <div class="d-lg-flex justify-content-between">
+                <div class="d-flex flex-column align-items-lg-start align-items-center gap-4">
+                    <div class="logo">
+                        <img src="./assets/images/logo.png" alt="">
+                    </div>
+
+                    <ul class="list d-flex flex-column gap-5 mb-0 p-0">
+                        <li class="d-flex align-items-end gap-3 fw-bold">
+                            <span class="text-primary fs-35">2500+</span>
+                            <span class="fs-63 text-white lh-1">EMPLOYEES</span>
+                        </li>
+
+                        <li class="d-flex align-items-end gap-3 fw-bold">
+                            <span class="text-primary fs-35">10+</span>
+                            <span class="fs-63 text-white lh-1">ACTIVITY SECTORS</span>
+                        </li>
+
+                        <li class="d-flex align-items-end gap-3 fw-bold">
+                            <span class="text-primary fs-35">25+</span>
+                            <span class="fs-63 text-white lh-1">YEARS OF EXPERIENCE</span>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="main__pic text-lg-start text-center mt-lg-0 mt-5">
+                    <img src="./assets/images/main-pic.png" alt="">
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <livewire:footer />
+
+</div>
