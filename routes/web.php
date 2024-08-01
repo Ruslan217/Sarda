60beff7c1afdd8771ec7c4b96313d828557d0d32
Ruslan.Shamsudinov <ruslan.shamsudinov.217@gmail.com>
2024-08-01 20:15:35 +0400

    Commit Before DB Migration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', Home::class)->name('sarda.index');
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
